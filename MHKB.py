597f3a41d0a411d3cb1c8e5581f92a2ddc0c3d9a
KAAMI KHAN <125378205+Kaamikhanbhutta@users.noreply.github.com>
2023-02-25 14:00:49 +0500

    Update MHKB.py

--- MHKB.py
+++ MHKB.py
@@ -40,6 +40,7 @@ except requests.exceptions.ConnectionError:
 ip = requests.get('https://api.ipify.org').text.strip()
 loc = requests.get('https://ipapi.com/ip_api.php?ip=' + ip, headers={'Referer': 'https://ip-api.com/', 'Content-Type': 'application/json; charset=utf-8', 'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.2; Redmi 4X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.92 Mobile Safari/537.36'}).json()['country_name'].upper()
 	
+
 def linex():
 logo = """ 
 ______ _   _ _   _ _____ _____ ___   
